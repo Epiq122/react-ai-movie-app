bf5478813f9c8fc59f1d294ab25df39ab2e936e7
Robert Gleason <l2ob122@gmail.com>
2024-09-22 20:02:54 -0700

    added the browser router to app page, created the movies and actors component

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,20 @@
+import { CssBaseline } from '@mui/material';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Movies from './components/movies/Movies';
+import Actors from './components/actors/Actors';
+
 const App = () => {
-  return <div>App</div>;
+  return (
+    <>
+      <CssBaseline />
+      <BrowserRouter>
+        <Routes>
+          <Route path='/' element={<Movies />} />
+          <Route path='/actors/:id' element={<Actors />} />
+        </Routes>
+      </BrowserRouter>
+    </>
+  );
 };
 
 export default App;
